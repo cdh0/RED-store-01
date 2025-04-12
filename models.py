20d2a16e4c82d5eab426ed86a7a69220bf6ef28a
Dhia Eddine <chetouanidhia@gmail.com>
2025-04-12 03:06:28 +0100

    Create models.py

---
+++ models.py
@@ -0,0 +1,6 @@
+class ProductInfo:
+    def init(self, title: str, price: str, link: str, image: str):
+        self.title = title
+        self.price = price
+        self.link = link
+        self.image = image
