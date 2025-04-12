8a6d09e62aabe05983949d0435b6b2a351191432
Dhia Eddine <chetouanidhia@gmail.com>
2025-04-12 03:40:04 +0100

    Delete models.py

--- models.py
+++
@@ -1,6 +0,0 @@
-class ProductInfo:
-    def init(self, title: str, price: str, link: str, image: str):
-        self.title = title
-        self.price = price
-        self.link = link
-        self.image = image
